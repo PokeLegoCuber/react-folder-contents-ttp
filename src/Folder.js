749ef4cbd37ee0e1949a6124a4cfa2c36bcc3d36
PokeLegoCuber <PokeLegoCuber@yahoo.com>
2020-06-03 14:03:29 -0400

    Folder Compontent

---
+++ src/Folder.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Folder() {
+  return (
+    <div>
+      <h1>Home &#x1F4C1;</h1>
+      <ul>
+          <li>File 1 &#x1F4C4;</li>
+          <li>File 2 &#x1F4C4;</li>
+          <li>File 3 &#x1F4C4;</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Folder;
